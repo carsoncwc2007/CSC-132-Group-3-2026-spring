5c3bc0c084d14cce78cfb7b79976d4208077bdb4
Carson Capdeboscq <Carson.cwc2007@gmail.com>
2026-04-18 15:10:46 -0500

    optimized code to make it run smoother

--- camera.py
+++ camera.py
@@ -1,16 +1,30 @@
+
 import numpy as np
 import cv2
 import mediapipe as mp
+#importing libraries 
 mp_hands=mp.solutions.hands
 mp_drawing=mp.solutions.drawing_utils
+#mediapipe setup
 cap = cv2.VideoCapture(0)
-print( "testing")
+#opening the webcam
+hands=mp_hands.Hands(
+static_image_mode=False,
+max_num_hands=2,
+min_detection_confidence=0.5,
+min_tracking_confidence=0.5
+)
+
 while True:
     ret, frame = cap.read()
-    #applies the model
+    if not ret:
+        print("Failed to grab frame")
+        break
+    
+    frame=cv2.flip(frame,1)
     
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results=mp_hands.Hands().process(frame)
+    results=hands.process(rgb_frame)
     #annotations the image
     cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
